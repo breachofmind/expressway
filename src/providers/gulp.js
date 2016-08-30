ff38e8ee5ea68ff6019ace3010b0908f2b8d8e47
Mike Adamczyk <mike.adamczyk@brightstar.com>
2016-08-30 11:38:56 -0500

    Change set method

--- src/providers/gulp.js
+++ src/providers/gulp.js
@@ -9,11 +9,26 @@ var mvc = require('../../index');
  */
 function Gulper(app)
 {
+    var gulp;
+
     this.paths = {
         build:  app.path('static_path', 'public'),
         scss:   app.path('resources_path', 'resources') + "scss",
         js:     app.path('resources_path', 'resources') + "js",
-        views:  app.path('views_path', 'resources/views')
+        lib:    app.path('resource_path', 'resources') + "js/vendor",
+        views:  app.path('views_path', 'resources/views'),
+        npm:    app.rootPath('node_modules'),
+    };
+
+    /**
+     * Set the gulp object.
+     * @param object
+     * @returns {Gulper}
+     */
+    this.set = function(object)
+    {
+        gulp = object;
+        return this;
     };
 
     /**
@@ -33,12 +48,35 @@ function Gulper(app)
      * @param pathArray array
      * @returns {*|Object|Array}
      */
-    this.pathsTo = function(name, pathArray)
+    this.collection = function(name, pathArray)
     {
         return pathArray.map(function(path) {
             return this.pathTo(name,path);
         }.bind(this))
     };
+
+    /**
+     * Create collections out the given path>array object.
+     * @param object
+     * @returns {{}}
+     */
+    this.collections = function(object)
+    {
+        Object.keys(object).forEach(function(path) {
+            object[path] = this.collection(path, object[path]);
+        }.bind(this));
+        return object;
+    };
+
+    /**
+     * Create a gulp dest to the build path.
+     * @param path string
+     * @returns {gulp}
+     */
+    this.dest = function(path)
+    {
+        return gulp.dest(this.paths.build+(path ? "/"+path : ""));
+    }
 }
 
 /**
