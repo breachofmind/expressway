dba06f8458aebb9636d1807ff6e08e92d84735fa
Mike Adamczyk <mike.adamczyk@brightstar.com>
2016-09-28 16:01:03 -0500

    Refactor locale class

--- src/providers/locale.js
+++ src/providers/locale.js
@@ -21,34 +21,171 @@ class LocaleProvider extends expressway.Provider
             'ExpressProvider'
         ];
 
-        this.inject = ['log','ExpressProvider'];
+        this.inject = ['ExpressProvider'];
     }
 
-    register(app,logger,ExpressProvider)
+    /**
+     * Register the provider with the application.
+     * @param app Application
+     * @param ExpressProvider
+     */
+    register(app,ExpressProvider)
     {
-        /**
-         * The main key store.
-         * @type {KeyStore}
-         */
-        var store = new KeyStore();
-        var langPath = app.conf('locales_path', 'lang');
+        var Localization = app.call(this,'getLocalizationClass', [app,'log']);
 
-        // Load the language files.
-        var langDirs = glob.sync(app.rootPath(langPath+'/*'));
+        var localization = new Localization();
 
-        langDirs.forEach(function(dirPath,i)
-        {
-            store.setLocaleDir(dirPath)
+        app.register('Localization', localization);
+
+        // When each view is created, add the template function.
+        app.event.on('view.created', function(view,request) {
+            view.data.lang = localization.helper(request);
         });
 
+        ExpressProvider.middleware(function localeMiddleware(app) {
+            return localization.middleware;
+        });
+    }
 
-        /**
-         * A collection of keys and values.
-         * @constructor
-         */
-        function KeyStore()
+    /**
+     * Return the localization class.
+     * @param app Application
+     * @param log Winston
+     * @returns {Localization}
+     */
+    getLocalizationClass(app,log)
+    {
+        return class Localization
         {
-            this.index = {};
+            constructor()
+            {
+                this.index = {};
+
+                this.langPath = app.conf('locales_path', 'lang');
+
+                this.langDirs = glob.sync(app.rootPath(this.langPath+'/*'));
+
+                this.langDirs.forEach(function(dirName) {
+                    this.setLocaleDir(dirName);
+                }.bind(this));
+            }
+
+            /**
+             * Index a locale directory, where the directory name is the locale name.
+             * @param dirName string
+             */
+            setLocaleDir(dirName)
+            {
+                var localeName = path.basename(dirName);
+                this.setLocale(localeName);
+
+                var files = utils.getFileBaseNames(dirName+"/");
+
+                files.forEach(function(file)
+                {
+                    var hash = require(dirName+"/"+file);
+
+                    var count = this.setKeys(localeName, file, hash);
+
+                    log.debug("[Locale] Loaded File: %s.%s (%d keys)", localeName, file, count);
+
+                }.bind(this));
+            }
+
+            /**
+             * Set a locale key.
+             * @param localeName string
+             * @param key string
+             * @param value string
+             * @returns {*}
+             */
+            setKey(localeName, key, value)
+            {
+                var locale = Localization.getLocaleValue(localeName);
+
+                return this.index[locale][key] = value;
+            };
+
+            /**
+             * Set multiple keys for a locale.
+             * @param localeName string
+             * @param file string
+             * @param hash object
+             * @returns {Number}
+             */
+            setKeys(localeName, file, hash)
+            {
+                var keys = Object.keys(hash);
+                for (let i=0; i< keys.length; i++) {
+                    this.setKey(localeName, `${file}.${keys[i]}`, hash[keys[i]]);
+                }
+                return keys.length;;
+            }
+
+            /**
+             * Get a locale key.
+             * @param localeName string
+             * @param key string
+             * @param args array
+             * @returns {*}
+             */
+            getKey(localeName, key, args)
+            {
+                var locale =  Localization.getLocaleValue(localeName);
+
+                if (this.hasLocale(locale)) {
+                    var value = this.index[locale][key];
+
+                    if (value) {
+                        return args ?  Localization.doReplace(value, args) : value;
+                    }
+                }
+
+                return "undefined:"+locale+"."+key;
+            };
+
+
+
+            /**
+             * Check if the locale exists in the index.
+             * @param localeName string
+             * @returns {boolean}
+             */
+            hasLocale(localeName)
+            {
+                return this.index.hasOwnProperty(  Localization.getLocaleValue(localeName) );
+            };
+
+            /**
+             * Return all the keys for the given locale.
+             * @param localeName string
+             * @returns {*|null}
+             */
+            getLocale(localeName)
+            {
+                return this.index[  Localization.getLocaleValue(localeName) ] || null;
+            };
+
+            /**
+             * Create a locale index.
+             * @param localeName string
+             * @returns {Localization}
+             */
+            setLocale(localeName)
+            {
+                this.index[  Localization.getLocaleValue(localeName) ] = {};
+                return this;
+            }
+
+            /**
+             * Return the locale name, or the default locale value.
+             * @param localeName string
+             * @returns {string}
+             */
+            static getLocaleValue(localeName)
+            {
+                return localeName.toLowerCase();
+            }
 
             /**
              * Replaces variables in the string in order.
@@ -56,7 +193,7 @@ class LocaleProvider extends expressway.Provider
              * @param args array|string
              * @returns {string}
              */
-            function doReplace(string, args)
+            static doReplace(string, args)
             {
                 if (! Array.isArray(args)) args = [args];
 
@@ -68,148 +205,35 @@ class LocaleProvider extends expressway.Provider
             }
 
             /**
-             * Return the locale name, or the default locale value.
-             * @param localeName string
-             * @returns {string}
+             * Return a helper function for retrieving a key.
+             * @param request
+             * @returns {Function}
              */
-            function getLocaleValue(localeName)
+            helper(request)
             {
-                return localeName.toLowerCase();
+                var locale = request.locale.toLowerCase();
+
+                return function(key,args) {
+                    if (! key || key=="") return "";
+                    if (! locale) locale = "en_us";
+
+                    return this.getKey(locale,key,args);
+
+                }.bind(this);
             }
 
             /**
-             * Set a locale key.
-             * @param localeName string
-             * @param key string
-             * @param value string
-             * @returns {*}
+             * Return some express middleware.
+             * @returns {function(this:Localization)}
              */
-            this.setKey = function(localeName, key, value)
+            get middleware()
             {
-                var locale = getLocaleValue(localeName);
-
-                return this.index[locale][key] = value;
-            };
-
-            /**
-             * Get a locale key.
-             * @param localeName string
-             * @param key string
-             * @param args array
-             * @returns {*}
-             */
-            this.getKey = function(localeName, key, args)
-            {
-                var locale = getLocaleValue(localeName);
-
-                if (this.hasLocale(locale)) {
-                    var value = this.index[locale][key];
-
-                    if (value) {
-                        return args ? doReplace(value, args) : value;
-                    }
-                }
-
-                return "undefined:"+locale+"."+key;
-            };
-
-            /**
-             * Create a locale index.
-             * @param localeName string
-             * @returns {KeyStore}
-             */
-            this.setLocale = function(localeName)
-            {
-                this.index[ getLocaleValue(localeName) ] = {};
-                return this;
-            };
-
-            /**
-             * Check if the locale exists in the index.
-             * @param localeName string
-             * @returns {boolean}
-             */
-            this.hasLocale = function(localeName)
-            {
-                return this.index.hasOwnProperty( getLocaleValue(localeName) );
-            };
-
-            /**
-             * Return all the keys for the given locale.
-             * @param localeName string
-             * @returns {*|null}
-             */
-            this.getLocale = function(localeName)
-            {
-                return this.index[ getLocaleValue(localeName) ] || null;
-            };
-
-            /**
-             * Index a locale directory, where the directory name is the locale name.
-             * @param dirName string
-             */
-            this.setLocaleDir = function(dirName)
-            {
-                var store = this;
-
-                var localeName = path.basename(dirName);
-                this.setLocale(localeName);
-
-                var files = utils.getFileBaseNames(dirName+"/");
-
-                files.forEach(function(file)
-                {
-                    var lang = require(dirName+"/"+file);
-
-                    var index=0;
-                    for (var prop in lang)
-                    {
-                        if (! lang.hasOwnProperty(prop)) {
-                            continue;
-                        }
-                        store.setKey(localeName, file+"."+prop, lang[prop]);
-                        index++;
-                    }
-                    logger.debug("[Locale] Loaded File: %s.%s (%d keys)", localeName, file, index);
-                });
-            }
-
-        }
-
-        /**
-         * The language function.
-         * @usage lang("fileName.propertyName", args)
-         * @param request
-         * @returns {Function}
-         */
-        function lang(request)
-        {
-            var locale = request.locale.toLowerCase();
-
-            return function(key, args) {
-                if (! key || key=="") {
-                    return "";
-                }
-                return store.getKey(locale||"en_us", key, args);
+                return function localeMiddleware(request,response,next) {
+                    request.lang = this.helper(request);
+                    next();
+                }.bind(this);
             }
         }
-
-        app.locale = store;
-
-        app.register('Localization', store);
-
-        // When each view is created, add the template function.
-        app.event.on('view.created', function(view,request) {
-            view.data.lang = lang(request);
-        });
-
-        // Add the helper function to the request object.
-        ExpressProvider.middleware(function localeMiddleware(app) {
-            return function(request,response,next) {
-                request.lang = lang(request);
-                next();
-            }
-        });
     }
 }
 
